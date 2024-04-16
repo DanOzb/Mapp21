ef3ce57aaec18badddb06ccfdbec58defa7aaa83
oesqar <kurdziel.oskar@gmail.com>
2024-04-16 14:00:53 +0200

    RoomController och VideoFootage
    Man kan enter o exit room 1

--- Escape room/Assets/Scripts/RoomController.cs
+++ Escape room/Assets/Scripts/RoomController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void EnterRoom()
     {
-        
+        SceneManager.LoadScene(2);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void GoBackToMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 }
