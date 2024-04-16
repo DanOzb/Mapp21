0925f1cca25d73baad031017473684fb3631ed6c
Danyal Özbek <enes.ozbek2002@gmail.com>
2024-04-16 18:04:51 +0200

    Rum scen video

---
+++ Escape room/Assets/Scripts/VideoController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayer : MonoBehaviour
+{
+    [SerializeField] VideoPlayer myVideoPlayer;
+    void Start()
+    {
+        
+    }
+
+    void ChangeScene(VideoPlayer vp)
+    {
+
+    }
+}
