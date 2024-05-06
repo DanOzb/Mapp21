22e0e8a19ac2569f47d0034171585290541bd4e4
oesqar <kurdziel.oskar@gmail.com>
2024-05-06 13:06:07 +0200

    Bakgrundsbild + knappar
    Ny bakgrundsbild

--- Escape room/Assets/Scripts/RageComplyVideos.cs
+++ Escape room/Assets/Scripts/RageComplyVideos.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -8,22 +7,15 @@ public class RageComplyVideos : MonoBehaviour
 {
     public VideoPlayer videoRage;
     public VideoPlayer videoComply;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Button btnComply = transform.Find("Comply").GetComponent<Button>();
-        btnComply.onClick.AddListener(PlayVideoComply);
+    
+   
 
-        Button btnRage = transform.Find("Rage").GetComponent<Button>();
-        btnRage.onClick.AddListener(PlayVideoRage);
-    }
-
-    void PlayVideoRage()
+    public void PlayVideoRage()
     {
         videoRage.Play();
     }
 
-    void PlayVideoComply()
+    public void PlayVideoComply()
     {
         videoComply.Play();
     }
