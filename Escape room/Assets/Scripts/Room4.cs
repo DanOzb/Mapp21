915a7ebe93f2e7b00c8811357ca4ea59295347d4
Danyal Özbek <enes.ozbek2002@gmail.com>
2024-05-24 16:19:39 +0200

    rum4 fixat

--- Escape room/Assets/Scripts/Room4.cs
+++ Escape room/Assets/Scripts/Room4.cs
@@ -6,87 +6,33 @@ using UnityEngine.Video;
 
 public class Room4 : MonoBehaviour
 {
-    [SerializeField] VideoPlayer endingA, endingB, endingC;
-    [SerializeField] GameObject endingAScreen, endingBScreen, endingCScreen, videoController;
+    [SerializeField] GameObject endingAScreen, endingBScreen, endingCScreen, videoController; 
 
     [SerializeField] GameObject canvas;
-    private float videoLength;
-    private string ending;
+    public string ending = "";
 
-    public void OptionA()
+    public void SetVideo(string AToC)
     {
-        videoController.transform.GetChild(1).gameObject.SetActive(false);
-        videoController.transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        canvas.SetActive(false);
-        if(endingA.clip != null)
-        {
-            endingA.gameObject.SetActive(true);
-            ending = "A";
-            videoLength = (float)(endingA.clip.length + 0.5);
-            endingA.Play();
-            Invoke("EndReached", videoLength);
-        }
-        else
-        {
-            endingAScreen.SetActive(true);
-        }
+        ending = AToC;
         
-    }
-
-    public void OptionB()
-    {
-        videoController.transform.GetChild(1).gameObject.SetActive(false);
-        videoController.transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        canvas.SetActive(false);
-        if (endingB.clip != null)
-        {
-            endingB.gameObject.SetActive(true);
-            ending = "B";
-            videoLength = (float)(endingB.clip.length + 0.5);
-            endingB.Play();
-            Invoke("EndReached", videoLength);
-        } else
-        {
-            endingBScreen.SetActive(true);
-        }
-        
-    }
-
-    public void OptionC()
-    {
-        videoController.transform.GetChild(1).gameObject.SetActive(false);
-        videoController.transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        canvas.SetActive(false);
-        if (endingC.clip != null)
-        {
-            endingC.gameObject.SetActive(true);
-            ending = "C";
-            videoLength = (float)(endingC.clip.length + 0.5);
-            endingC.Play();
-            Invoke("EndReached", videoLength);
-        } else
-        {
-            endingCScreen.SetActive(true);
-        }
-
     }
 
     //spelar en video innan game over screen
-    private void EndReached()
+    public void EndReached()
     {
         switch (ending)
         {
             case "A":
-                endingA.gameObject.SetActive(false);
                 endingAScreen.SetActive(true);
+                ending = "";
                 break;
             case "B":
-                endingB.gameObject.SetActive(false);
                 endingBScreen.SetActive(true);
+                ending = "";
                 break;
             case "C":
-                endingC.gameObject.SetActive(false);
                 endingCScreen.SetActive(true);
+                ending = "";
                 break;
         }
     }
