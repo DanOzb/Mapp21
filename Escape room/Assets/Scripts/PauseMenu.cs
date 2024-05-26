b7d899a66dfcb8253a6be65484e4ece5bbe5a006
Danyal Özbek <enes.ozbek2002@gmail.com>
2024-05-26 14:50:21 +0200

    tagit bort extra scener

--- Escape room/Assets/Scripts/PauseMenu.cs
+++ Escape room/Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
 public class PauseMenu : MonoBehaviour
 {
